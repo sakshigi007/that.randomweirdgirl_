a16f74cfbb8a8e6b05e4e7c6dbefb862c8cb1c02
sakshigi007 <sakshigi007@gmail.com>
2020-04-17 16:41:42 +0530

    Create simplecalculator.py
    My first simple calculator code

---
+++ simplecalculator.py
@@ -0,0 +1,31 @@
+print("select operation:\n"
+        "1. add\n"
+        "2. subtract\n"
+        "3. multiply\n"
+        "4. divide\n")
+
+a=int(input("enter a number:\n"))
+b=int(input("enter another number:\n"))
+
+select=input("enter a number(1/2/3/4)")
+if select=='1':
+    print(a,"+",b,"=",add(a,b))
+elif select=='2':
+    print(a,"-",b,"=",subtract(a,b))
+elif select=='3':
+    print(a,"*",b,"=",multiply(a,b))
+elif select=='4':
+    print(a,"/",b,"=",divide(a,b))
+else:
+    print("invalid")
+
+
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    return a/b
+  
