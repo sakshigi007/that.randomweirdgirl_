9984f9520e13398b26821c196efbfbfc95bba21f
sakshigi007 <sakshigi007@gmail.com>
2020-04-20 22:25:03 +0530

    Create addTwoNumbers.py

---
+++ addTwoNumbers.py
@@ -0,0 +1,10 @@
+# Ask a user to enter a number
+num1=int(input("enter a number:\n"))
+# Ask a user to enter a second number
+num2=int(input("enter another number:\n"))
+# Calculate the total of the two numbers added together
+print('addition')
+# Print 'first number + second number = answer' 
+print(num1 + num2)
+# For example if someone enters 4 and 6 the output should read
+# 4 + 6 = 10
